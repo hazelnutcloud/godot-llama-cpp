0dcb26e7de363a5ce04ff815f0306cc89dd7af6e
Hasan Mukhlis <hsnmkls@proton.me>
2024-02-29 01:45:53 +0800

    revert const

--- godot/addons/godot-llama-cpp/autoloads/llama-backend.gd
+++ godot/addons/godot-llama-cpp/autoloads/llama-backend.gd
@@ -1,7 +1,7 @@
 # This script will be autoloaded by the editor plugin
 extends Node
 
-const backend: LlamaBackend = LlamaBackend.new()
+var backend: LlamaBackend = LlamaBackend.new()
 
 func _enter_tree() -> void:
   backend.init()
