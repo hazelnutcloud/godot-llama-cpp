0f39d3d932a1bc1090c8bfeaa7499f5129e03519
Hasan Mukhlis <hsnmkls@proton.me>
2024-02-29 01:35:07 +0800

    add simple ui

--- godot/main.gd
+++ godot/main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var input: TextEdit = $"Form/Input"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_pressed():
+	print(input.text)
